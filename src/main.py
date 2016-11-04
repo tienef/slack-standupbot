109ac294a8af888e1a64299138595b5e91c3e451
t.faivre <t.faivre.ext@paritel.fr>
2016-11-04 12:04:40 +0100

    Mise en paramètre du [serveur, user, pwd] Confluence et du token Slack

--- src/main.py
+++ src/main.py
@@ -4,22 +4,30 @@ from standup.bot import StandUpBot
 import argparse
 
 # Définition et lecture des arguments
-parser = argparse.ArgumentParser(description="Connecte le standup-bot au channel -c demandé et recueille les rapports de jour au format \"today xxxx\", \"yesterday xxxx\" "
-                                             "ou \"obstacles xxxx\" et les enregistre dans le Confluence de Kepler dans l'espace -s sous la page -p demandée")
-parser.add_argument("-c", "--channel", help="Nom du channel Slack où le bot doit se connecter pour lire les rapports du jour")
-parser.add_argument("-s", "--space", help="Clé spaceKey de l'espace Confluence dans lequel stocker les rapports du jour")
-parser.add_argument("-p", "--page", help="Nom de la page dans Confluence sous laquel les rapports seront stockés")
+parser = argparse.ArgumentParser(description="Connects standup-bot to -c channel of Slack using -t token. Collects"
+                                             " messages of the day having format \"today xxxx\", \"yesterday xxxx\" "
+                                             "or \"obstacles xxxx\" and save them in Confluence -s server in space "
+                                             "having -k key under page name -n. -u User and -p Password for Confluence"
+                                             " must also be provided. Designed for Confluence 5.9.2")
+parser.add_argument("-c", "--channel", help="Name of Slack channel where bot must connect to collect messages")
+parser.add_argument("-k", "--key", help="SpaceKey of Confluence space where daily reports will be generated")
+parser.add_argument("-n", "--nameofconfluencepage", help="Name of Confluence page under daily reports will be generated")
+parser.add_argument("-t", "--token", help="Slack token")
+parser.add_argument("-s", "--serverconfluence", help="Confluence Server. Example : \"https://docs.myconfluenceserver.com\"")
+parser.add_argument("-u", "--userconfluence", help="Confluence user to connect with")
+parser.add_argument("-p", "--passwordconfluence", help="Password password to connect with")
+
 args = parser.parse_args()
 
 # Slack
-slackToken = "x"
+slackToken = args.token
 slackServer = "https://slack.com"
 slackChannel = args.channel
 
 # Confluence
-conflServer = "https://docs.kepler-rominfo.com"
-conflUser = "x"
-conflPassword = "x"
+conflServer = args.serverconfluence
+conflUser = args.userconfluence
+conflPassword = args.passwordconfluence
 conflSpaceKey = args.space
 conflMainContentName = args.page
 
